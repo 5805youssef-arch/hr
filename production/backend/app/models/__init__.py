534aa000b0da680016c18b46bbaa3f7af471fb6f
Yahia20 <yz1126@fayoum.edu.eg>
2026-04-17 09:39:19 +0200

    chore: initial project skeleton (FastAPI + Docker Compose)

--- production/backend/app/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .employee import Employee
-from .violation import Violation
-from .rule import Rule
--- production/backend/app/schemas/__init__.py
+++
@@ -1,3 +0,0 @@
-from .employee import EmployeeCreate, EmployeeResponse
-from .violation import ViolationCreate, ViolationResponse
-from .rule import RuleCreate, RuleResponse
